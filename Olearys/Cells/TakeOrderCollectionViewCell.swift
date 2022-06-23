71ee69d92bfe092fea027dbf9095965de9cdfc74
moussa <moussa.ellakki@hotmail.com>
2022-06-23 20:43:01 +0200

    Fixat CollectionView for takeOrderView

---
+++ Olearys/Cells/TakeOrderCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TakeOrderCollectionViewCell.swift
+//  Olearys
+//
+//  Created by Moussa El Lakki on 2022-06-23.
+//
+
+import UIKit
+
+class TakeOrderCollectionViewCell: UICollectionViewCell {
+  
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    
+}
--- Olearys/Controller/TakeOrderViewController.swift
+++ Olearys/Controller/TakeOrderViewController.swift
@@ -1,14 +1,36 @@
 
 import UIKit
 
-class TakeOrderViewController: UIViewController {
+class TakeOrderViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource {
+  
+    
 
+    @IBOutlet weak var menuColleCtionView: UICollectionView!
+    
+    var forDelete = [1,2,3,4,5,6,7,8,9,10,11,12]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        menuColleCtionView.delegate = self
+        menuColleCtionView.dataSource = self
     
     }
     
 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return forDelete.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "takeOrderCell", for: indexPath) as! TakeOrderCollectionViewCell
+        
+        cell.typeLabel.text = "Food mat\(forDelete[indexPath.row])"
+        return cell
+        
+    }
+    
 
 }

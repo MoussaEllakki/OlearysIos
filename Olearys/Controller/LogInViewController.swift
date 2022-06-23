01c716af392998bb3564a74d0a47b6be040d4dd3
moussa <moussa.ellakki@hotmail.com>
2022-06-23 10:02:43 +0200

    Fixat log in

--- Olearys/Controller/LogInViewController.swift
+++ Olearys/Controller/LogInViewController.swift
@@ -19,7 +19,7 @@ class LogInViewController: UIViewController {
 
         ref = Database.database().reference()
    
-        ref.child("xxxxxxx").setValue("mmmmmmmmmmm")
+        ref.child("xxxxxxx").setValue("yesssss")
         //testa
         
     }
